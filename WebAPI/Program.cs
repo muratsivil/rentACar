91117a0b0ed55912c8cf37b301e5c0f556c442fa
Murat Sivil <murat.sivil@im-par.de>
2024-02-27 01:17:11 +0300

    PersistenceServiceRegistration added

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,4 +1,5 @@
 using Application;
+using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddApplicaitonServices();
+builder.Services.AddPersistenceServices(builder.Configuration);
 builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddDistributedMemoryCache();

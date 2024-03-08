ff34beed3f7da5cb36a29c5d9d2dd46a5b61ee5f
Murat Sivil <murat.sivil@im-par.de>
2024-03-08 09:55:17 +0300

    Add new DbSet properties to BaseDbContext

--- Persistence/Contexts/BaseDbContext.cs
+++ Persistence/Contexts/BaseDbContext.cs
@@ -1,16 +1,26 @@
-﻿using System.Reflection;
-using Domain.Entites;
+﻿using Domain.Entites;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Persistence.Contexts;
 
-public class BaseDbContext : DbContext
+public class BaseDbContext:DbContext
 {
-    protected IConfiguration Configuration {get; set;}
+    protected IConfiguration Configuration { get; set; }
     public DbSet<Brand> Brands { get; set; }
-    
-    public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions){
+    public DbSet<Car> Cars { get; set; }
+    public DbSet<Fuel> Fuels { get; set; }
+    public DbSet<Transmission> Transmissions { get; set; }
+    public DbSet<Model> Models { get; set; }
+
+    public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions)
+    {
         Configuration = configuration;
         Database.EnsureCreated();
     }
@@ -18,6 +28,5 @@ public class BaseDbContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
-
     }
 }
